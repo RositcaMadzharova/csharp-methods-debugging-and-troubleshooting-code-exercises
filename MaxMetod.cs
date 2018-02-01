178f41687df7fd69bf14b013b58439172ddba4e4
RositcaMadzharova <val.ros@abv.bg>
2018-02-01 19:46:04 +0200

    -

---
+++ MaxMetod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Max_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int biggest = GetMax(first, Math.Max(second, third));
+            Console.WriteLine(biggest);
+        }
+
+        static int GetMax(int first, int second)
+        {
+            if (first > second)
+            {
+                return first;
+            }
+
+            else return second; 
+        }
+    }
+}
