b54ae1df881284e9b32f863c06b7bdfde6899627
RositcaMadzharova <val.ros@abv.bg>
2018-02-04 12:59:16 +0200

    -

---
+++ Numbers in Reversed Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Numbers_in_Reversed_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal inputNumber = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(ReverseDecimal(inputNumber));
+        }
+
+        static decimal ReverseDecimal(decimal input)
+        {
+            string stringRepresentation = input.ToString();
+            string reversedString = "";
+            for (int i = stringRepresentation.Length - 1; i >= 0; i--)
+            {
+                reversedString += stringRepresentation[i];
+            }
+            return decimal.Parse(reversedString);
+        }
+    }
+}
