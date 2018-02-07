e53ff5a4d1c91147e8cbf0a7ed419294b61d6fac
RositcaMadzharova <val.ros@abv.bg>
2018-02-07 20:02:43 +0200

    -

---
+++ Prime Checker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Prime_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong number = ulong.Parse(Console.ReadLine());
+            Console.WriteLine(IsPrime(number));
+
+        }
+
+        static bool IsPrime(ulong number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return false;
+            }
+
+            bool isPrime = false;
+
+            for (ulong i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
