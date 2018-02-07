9a936d482cbe0374d4d384d4c124fc9951beff65
RositcaMadzharova <val.ros@abv.bg>
2018-02-07 19:26:48 +0200

    -

---
+++ Fibonacci Numbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fibonacci_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int result = CalculateFibonacci(number);
+            Console.WriteLine(result); ;
+
+        }
+
+        static int CalculateFibonacci(int number)
+        {
+            int result = 0;
+
+            if (number == 0 || number == 1)
+            {
+                result = 1;
+                return result;
+            }
+
+            int previousNunber = 1;
+            int beforePrevious = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result = previousNunber + beforePrevious;
+                beforePrevious = previousNunber;
+                previousNunber = result;
+            }
+
+            return result;
+        }
+    }
+}
